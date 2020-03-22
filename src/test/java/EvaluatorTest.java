fff7adffece8060df1065ace93416e2c7f79af38
Nikolay <smir.nick2000@gmail.com>
2020-03-22 22:29:29 +0300

    Add tests

--- src/test/java/EvaluatorTest.java
+++ src/test/java/EvaluatorTest.java
@@ -13,5 +13,7 @@ class EvaluatorTest {
 		assertEquals(evaluator.eval("1 + sin(3 * 3.1415926535)"), 1.0, 1e-5);
 		assertEquals(evaluator.eval("pow(3 + 1, 3 -1)"), 16.0, 1e-5);
 		assertEquals(evaluator.eval("cos(sin(1))"), 0.66636674, 1e-5);
+
+		assertEquals(evaluator.eval("pow(cos(0.3), 2.0) + pow(sin(0.3), 2.0)"), 1.0, 1e-5);
 	}
 }
